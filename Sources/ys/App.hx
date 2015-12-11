98d11e16020e24853e89d5af357936b161d2a411
wighawag <wighawag@gmail.com>
2015-12-11 12:41:08 +0000

    make us of new kha.System and Scheduler for app start

--- Sources/ys/App.hx
+++ Sources/ys/App.hx
@@ -1,7 +1,7 @@
 package ys;
 
-import kha.Starter;
-import kha.Game;
+import kha.System;
+import kha.Scheduler;
 
 typedef Framebuffer = kha.Framebuffer;
 
@@ -12,30 +12,49 @@ interface Application{
 }
 
 @:access(ys.Input)
-class App extends Game{
+class App {
   public static function start(name : String, app : Application){
-    var starter = new Starter();
-		starter.start(new App(name, app));
+    if(_startedApplication == null){
+      _startedApplication = app;
+      if(_startedApplication != null){
+        System.init(name, 640, 480, init); //TODO dimensions    
+      }else{
+        //TODO error?
+      }
+    }else{
+      //TODO error?
+    }
   }
+
+  static var _startedApplication : Application;
+  static var _instance : App;
+
+  static function init() {
+    _instance = new App(_startedApplication);
+    _instance.initialise();
+    System.notifyOnRender(_instance.render);
+    Scheduler.addTimeTask(_instance.update, 0, 1 / 60); //TODO interval
+  }
+
+
 
   var _lastNow : Float;
   var _app : Application;
   var _input : Input;
   
-  private function new(name : String, app : Application){
-    super(name,false);
+  private function new(app : Application){
     _app = app;
     _input = new Input(kha.input.Keyboard.get(0), kha.input.Mouse.get(0), kha.input.Gamepad.get(0));
   }
 
-  override public function init(){
-    _lastNow = kha.Sys.getTime();
+  private function initialise(){
+    _lastNow = kha.System.time;
     _app.init(_lastNow, _input);
   }
 
 
-  override public function update(){
-    var now = kha.Sys.getTime();
+  private function update(){
+    var now = kha.System.time;
     var dt = now - _lastNow;
     _lastNow = now;
     _input.preUpdate();
@@ -43,7 +62,7 @@ class App extends Game{
     _input.postUpdate();
   }
 
-  override public function render(framebuffer : Framebuffer){
-    _app.render(kha.Sys.getTime(),framebuffer, _input);
+  private function render(framebuffer : Framebuffer){
+    _app.render(kha.System.time,framebuffer, _input);
   }
 }
